12cb505afa6d469c8f30f965bd60cc97cb93f5ef
Abby Farhat <abbysfarhat@gmail.com>
2026-01-26 23:01:41 -0500

    chore: reload vite whenever source packages change

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        {
+            name: 'force-reload-on-folder-change',
+            handleHotUpdate({ file, server }) {
+                const targetDir = path.resolve(__dirname, '../../packages');
+
+                if (file.startsWith(targetDir)) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                    return [];
+                }
+            },
+        },
+    ],
 });
